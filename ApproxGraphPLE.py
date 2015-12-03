4be720b710ad9c2f4e20f0dccbbf21d471dedc49
d1laj <dimitri.lajou@ens-lyon.fr>
2015-12-03 10:02:12 +0100

    inprovement on the graph construction: not finished yet

--- ApproxGraphPLE.py
+++ ApproxGraphPLE.py
@@ -1,51 +1,37 @@
-import numpy as np
 import math
-import operator
 
 
-class ApproxGraphPLE_Node:
+class Node:
     """Node from the approximation graph of the PLE algorithm"""
-    # Nodes' coordinates
-    x = float
-    y = float
-    z = float
-    # State of the node in the graph
-    disable = bool
-    
-    def __init__(self, x=0., y=0., z=0., disable=false):
+    def __init__(self, x=0., y=0., z=0., disable=False):
         self.x = x
         self.y = y
         self.z = z
         self.disable = disable
         
         
-class ApproxGraphPLE_Edge:
+class Edge:
     """Edge from the approximation graph of the PLE algorithm"""
-    # Min distance on the edge
-    base_distance = float
-    # Distance of the edge at time t
-    distance = float
-    
-    def __init__(self, base_distance, node1, node2):
+    def __init__(self, base_distance, node1, node2, disable=False):
         self.base_distance = base_distance
         self.distance = base_distance
         self.nodes = [node1, node2]
-
-Node = ApproxGraphPLE_Node
-Edge = ApproxGraphPLE_Edge
+        self.disable = disable
 
 
-class ApproxGraphPLE:
+class Graph:
     """The class reprensenting the approximation graph for the PLE algorithm"""
   
     def __init__(self, dx=0., sizeX=0., sizeY=0., posX=0., posY=0.):
         self.nodes = []  # List of the nodes
         self.edges = {}
         self.dx = dx
-        self.sizeX = sizeX.
+        self.sizeX = sizeX
         self.sizeY = sizeY
         self.posX = posX
         self.posY = posY
+        self.nX = 0
+        self.nY = 0
     
     def distance_euclidian(node1, node2):
         """Heuristic distance"""
@@ -56,16 +42,55 @@ class ApproxGraphPLE:
     def distance(node1, node2):
         """Distance used in the A* algorithm"""
         distance_euclide(node1, node2)
-    
-    def add(self, node, edges):
-        """Temporary way to add nodes"""
-        self.nodes = self.nodes.append(node)
-        self.edges[node] = edges
         
-    def create(self):
-        """Create a plan graph according to parameters"""
-        node=Node()
+    def create(self, sizeX, sizeY, d, posX, posY, base_distance):
+        """Create a grid plane graph according to parameters"""
+        self.nX = math.floor(sizeX / d) + 1
+        self.nY = math.floor(sizeY / d) + 1
+        self.nodes = [Node()]
+        self.edges = {}
+        for i in range(0, self.nX - 1):
+            for j in range(0, self.nY - 1):
+                self.nodes.append(Node(x=posX + d * i, y=posY + d * j))
+        for i in range(0, self.nX - 1):
+            for j in range(0, self.nY - 1):
+                self.edges[self.node[1 + i + j * self.sizeX]] = {}
+                my_node = self.node[1 + i + j * self.sizeX]
+                my_neighboors = self.edge[my_node]
+                if i > 0:
+                    neighboor = self.node[i + j * self.sizeX]
+                    my_neighboors[neighboor] = Edge(base_distance, my_node, neighboor)
+                if i < self.nX - 1:
+                    neighboor = self.node[2 + i + j * self.sizeX]
+                    my_neighboors[neighboor] = Edge(base_distance, my_node, neighboor)
+                if j > 0:
+                    neighboor = self.node[1 + i + (j - 1) * self.sizeX]
+                    my_neighboors[neighboor] = Edge(base_distance, my_node, neighboor)
+                if j < self.nY - 1:
+                    neighboor = self.node[i + (j + 1) * self.sizeX]
+                    my_neighboors[neighboor] = Edge(base_distance, my_node, neighboor)
+                self.edges[my_node] = my_neighboors
+
+    def find_neighboors(posX, posY):
+        """Find the nodes of the grid the closest to this position"""
+
+    def add_entry_point(self, posX, posY, posZ=0., base_distance):
+        """Add the individual in the graph"""
+        my_neighboors = self.find_neighboors(posX, posY)
+        me = Node(x=posX, y=posY, z=posZ)
+        self.nodes[0] = me
+        for neighnoor in my_neighboors:
+            new_edge = Edge(base_distance, me, neighboor)
+            self.edges[me][neighboor] = new_edge
+            self.edges[neighboor][me] = new_edge
     
+    def remove_entry_point(self):
+        """Remove the individual position"""
+        indiv = self.nodes[0]
+        for neighboor, edge in self.edges[indiv].items():
+            del self.edges[neighboor][indiv]
+        del self.edges[indiv]
+            
     def smallest_path_a_star(self, start, goal):
         """A* algorithm"""
         to_evaluate = {start: distance(start, goal)}  # Dictionary of the nodes to evaluate and the approximated distance to the goal
@@ -80,8 +105,8 @@ class ApproxGraphPLE:
             to_evaluate.pop(current)
             seen[current] = 1  # Adding current into the dictionary (the value doesn't interest us)
             
-            for (neighbor, edge) in self.edges[current]:
-                if neighbor.disable:
+            for (neighbor, edge) in self.edges[current].items():
+                if neighbor.disable or edge.disable:
                     continue
                 if neighbor in seen:
                     continue
