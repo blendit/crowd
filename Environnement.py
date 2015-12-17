171944ed992fb39a00267fface6281647263dc27
d1laj <dimitri.lajou@ens-lyon.fr>
2015-12-17 09:50:55 +0100

    recent changes

--- Environnement.py
+++ Environnement.py
@@ -4,5 +4,10 @@ import sympy
 # You shall not pass!!!!
     
     def is_disable_node(self, node):
+        for exclusion in self.mine_field:
+            if exclusion.encloses_point(Point(node.x, node.y)):
+                node.disable = True
+                return 0
     
-    def is_disable_edge(self, edge):
+    def is_disable_edge(self, edge, radius):
+        
