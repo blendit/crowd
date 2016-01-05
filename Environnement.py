4aeffe0159ff5fda57676ffdb509e816e2408d22
Johanna Seif <johanna.seif@ens-lyon.fr>
2016-01-05 13:33:52 +0100

    Environment class added

--- Environnement.py
+++ Environnement.py
@@ -1,4 +1,13 @@
-import sympy
+import shapely
+
+
+class PathEnvironnement:
+    """The class independant from Blender describing the environment in which the crowd moves"""
+    def __init__(self, ground, mine_field):
+        self.ground = ground
+        self.mine_field = mine_field # mine_field is a set of forbidden surfaces
+
+
 
 
 # You shall not pass!!!!
