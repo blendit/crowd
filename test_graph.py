fc5e9a75227ec76098443c98c8851c50c35fd64b
Etienne Moutot <etienne.moutot@ens-lyon.fr>
2016-01-07 09:12:06 +0100

    Tests for Graph class

--- test_graph.py
+++ test_graph.py
@@ -47,6 +47,22 @@ class TestClassEdge(unittest.TestCase):
         e.base_distance_gen(10.)
         e.init_distance()
         self.assertEqual(e.distance, dist)
+
+
+class TestClassGraph(unittest.TestCase):
+    def test_smallest_path(self):
+        g = Graph(2, 10, 10)
+        g.add_entry_point(0, 0)
+        g.add_goal_point(10, 10)
+
+        self.assertEqual(g.smallest_path_a_star(), 10*sqrt(2))
+        
+        g.remove_entry_point()
+        g.remove_goal_point()
+        g.add_entry_point(1, 1)
+        g.add_goal_point(9, 9)
+
+        self.assertEqual(g.smallest_path_a_star(), 8*sqrt(2))
         
 
 if __name__ == '__main__':
