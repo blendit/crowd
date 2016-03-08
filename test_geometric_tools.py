f48ed84e605436fccc4abb201bfa3c8eab3418d9
d1laj <dimitri.lajou@ens-lyon.fr>
2016-03-08 13:43:29 +0100

    Cleaned up test_geometric_tools.py

--- test_geometric_tools.py
+++ test_geometric_tools.py
@@ -188,11 +188,11 @@ class AffichePolygon():
         ax.set_xlim(*xrange)
         ax.set_ylim(*yrange)
         ax.set_aspect(1)
-        # plt.show()
+        plt.show()
 
 
 class AfficheLineString():
-    """Plot a polygon"""
+    """Plot a LineString"""
     def plot_poly(self, poly, xrange=[-10, 10], yrange=[-10, 10]):
         x, y = poly.exterior.xy
         
@@ -204,100 +204,4 @@ class AfficheLineString():
         ax.set_xlim(*xrange)
         ax.set_ylim(*yrange)
         ax.set_aspect(1)
-        # plt.show()
-
-"""
-T = TestGeometricTools()
-T.test_angle()
-print("###")
-#T.test_create_cone()
-#T.test_create_truncate_cone()
-T.test_find_projection_segment()
-print("###")
-T.test_find_projection_half_line()
-print("###")
-T.test_in_half_plane()
-print("###")
-T.test_intersection_line_line()
-
-
-tau = 0.5
-vmax = 10
-dmax = vmax * tau
-rA = rB = 4
-pA = S.Point(-12,6)
-pB = S.Point(0,0)
-A = AffichePolygon()
-
-print(distance(pA,pB))
-cone = TruncatedCone(pA, rA, pB, rB, dmax, tau)
-plt.plot([pB.x -pA.x,0], [pB.y -pA.y,0], 'ro')
-plt.plot([(pB.x -pA.x) /tau,0], [(pB.y -pA.y) /tau,0], 'ro')
-
-
-center = S.Point(pB.x - pA.x, pB.y - pA.y)
-theta_c = argument(center)
-norm = distance(center, S.Point(0, 0))
-r = rA + rB
-
-if norm > 0:
-    cos_theta = math.sqrt(norm ** 2 - r ** 2) / norm
-    sin_theta = r / norm
-    norm2 = math.sqrt(norm ** 2 - r ** 2)
-    # Trigo !
-    point1 = S.Point(norm2 * (math.cos(theta_c) * cos_theta + sin_theta * math.sin(theta_c)), norm2 * (math.sin(theta_c) * cos_theta - sin_theta * math.cos(theta_c)))
-    point2 = S.Point(norm2 * (math.cos(theta_c) * cos_theta - sin_theta * math.sin(theta_c)), norm2 * (math.sin(theta_c) * cos_theta + sin_theta * math.cos(theta_c)))
-    plt.plot([point1.x,point2.x], [point1.y,point2.y], 'go')
-    norm2 = math.sqrt(norm ** 2 - r ** 2) / tau
-    # Trigo !
-    point1 = S.Point(norm2 * (math.cos(theta_c) * cos_theta + sin_theta * math.sin(theta_c)), norm2 * (math.sin(theta_c) * cos_theta - sin_theta * math.cos(theta_c)))
-    point2 = S.Point(norm2 * (math.cos(theta_c) * cos_theta - sin_theta * math.sin(theta_c)), norm2 * (math.sin(theta_c) * cos_theta + sin_theta * math.cos(theta_c)))
-    plt.plot([point1.x,point2.x], [point1.y,point2.y], 'yo')
-
-
-
-coords = list(cone.arc.coords)
-coords_x = []
-coords_y = []
-for i in coords:
-    coords_x.append(i[0])
-    coords_y.append(i[1])
-    
-plt.plot(coords_x, coords_y, 'b.')
-A.plot_poly(center.buffer(r), xrange = [-30, 30], yrange = [-30, 30])
-A.plot_poly(S.Point((pB.x -pA.x) /tau, (pB.y -pA.y) /tau).buffer(r/tau), xrange = [-30, 30], yrange = [-30, 30])
-
-class Indiv:
-    def __init__(self, i):
-        if i == 1:
-            self.x = -12
-            self.y = 6
-            self.vmax = 10
-            self.v = S.Point(-10, -5)
-            self.radius = 4
-        else:
-            self.x = 0
-            self.y = 0
-            self.vmax = 10
-            self.v = S.Point(0, 10)
-            self.radius = 4
-    
-    
-ind = Indiv(0)
-nei = Indiv(1)
-
-v_opt = (ind.v.x - nei.v.x, ind.v.y - nei.v.y)
-u = cone.find_closest(v_opt)
-plt.plot([u.x,v_opt[0]], [u.y,v_opt[1]], 'mo')
-u = S.Point(u.x - v_opt[0], u.y - v_opt[1])
-
-print(u.x, u.y)
-origin = S.Point(1/2 * u.x + ind.v.x, 1/2 * u.y + ind.v.y)
-plt.plot([origin.x], [origin.y], 'co')
-
-V = VelocityField(ind, tau, [])
-#poly = V.orca(nei, tau)
-poly = half_plane(origin, u, ind.vmax)
-A.plot_poly(poly, xrange = [-30, 30], yrange = [-30, 30])
-plt.show()
-"""
+        plt.show()
