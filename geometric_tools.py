9d04195f1ff06fb1d6dc5113f87df4f122fe21fa
Johanna Seif <johanna.seif@ens-lyon.fr>
2016-03-10 21:49:08 +0100

    Function to compute the minimum energy

--- geometric_tools.py
+++ geometric_tools.py
@@ -12,26 +12,38 @@ def intersection_not_empty(obj1, obj2):
         return True
 
 
-def find_closest_to_optimal(d, pA, pB):
-    """Find the point(s) in [AB] whose distance to zero is the closest to d"""
-    p0 = S.Point(0, 0)
-    circle = p0.buffer(d).exterior
-    line = S.LineString([(pA.x, pA.y), (pB.X, pB.y)])
-    if intersection_not_empty(circle, line):
-        return circle.intersection(line)
-    else:
-        # test if p0 is the point we want (special case)
-        dist = circle.distance(line)
-        circle_dist = circle.buffer(dist).exterior
-        return circle_dist.intersection(line)
-
-
-def find_closest_to_optimal(vopt, obj1, center, angle)
-    """Find the point of the polygone obj1 at angle that is the closest to vopt"""
+def find_closest_to_optimal(vopt, obj1, center, angle):
+    """Find the point of the polygone obj1 (authorised speeds) at angle that is the closest to vopt"""
     xopt = math.cos(angle)
     yopt = math.sin(angle)
     Popt = S.Point(xopt, yopt)
     if intersection_not_empty(obj1, Popt):
         return Popt
     else:
-        line = S.LineString([(Popt.x, Popt.y), (center.X, center.y)])
+        line = S.LineString([(Popt.x, Popt.y), (center.x, center.y)])
+        obj1_ext = obj1.exterior
+        return line.intersection(obj1_ext)
+
+
+def dist_theta(vopt, obj1, center, angle, tau, indiv):
+    Pind = find_closest_to_optimal(vopt, obj1, center, angle)
+    vind = math.sqrt(Pind.x * Pind.x + Pind.y * Pind.y)
+    dist = vind * tau
+    xnew = center.x + dist * math.cos(angle)
+    ynew = center.y + dist * math.sin(angle)
+    L = 0.0  # Result of A*
+    energy = tau * (indiv.es + indiv.ew * vind * vind) + 2 * L * math.sqrt(indiv.es * indiv.ew)
+    return energy
+
+
+def best_angle(vopt, obj1, center, tau, dtheta, indiv):
+    act_ang = 0
+    min_energy = dist_theta(vopt, obj1, center, act_ang, tau, indiv)
+    best_ang = 0
+    while (ang < 2 * math.pi):  # If angles are not in degree
+        act_ang += dtheta
+        energy = dist_theta(vopt, obj1, center, act_ang, tau, indiv)
+        if energy < min_energy:
+            min_energy = energy
+            best_ang = act_ang
+    return (best_ang, min_energy)
