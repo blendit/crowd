c9956b8b03ccc8dbbef4e7820b8f9ea44eecfeb2
PijusSimonaitis <pijus.simonaitis@gmail.com>
2016-03-11 09:01:47 +0100

    Some errors debuged

--- path_creation_bezier.py
+++ path_creation_bezier.py
@@ -1,3 +1,5 @@
+import bpy
+
 # creating a simple BEZIER path that starts on point *initial* and points are taken from list *S*
 # we switched to bezier curve for many reasons but the main one is that bezier curves are an interpolation of the given points (the path actually include the parameter points) when nurbs curves are an approximation of the given points (the path does not go through the parameter points)
 initial = (0, 0, 0)
@@ -8,13 +10,14 @@ S = [
     (8, 7, 0)
 ]
 
-bpy.ops.curve.primitive_bezier_curve_add(view_align=False, enter_editmode=True, location=(initial[0] + 1, initial[1], initial[2]), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-
+bpy.ops.curve.primitive_bezier_curve_add(view_align=False, enter_editmode=True, location=(initial[0] - 1, initial[1], initial[2]))
 bpy.ops.curve.select_all()
-bpy.ops.curve.de_select_last()
-bpy.ops.curve.delete()
 bpy.ops.curve.de_select_last()
 
 for x in S:
     print(x)
     bpy.ops.curve.vertex_add(location=x)
+
+bpy.ops.curve.select_all()
+bpy.ops.curve.de_select_first()
+bpy.ops.curve.delete()
