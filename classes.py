abed3ea51f7a1256c3ef524a69c3b7717b6f3bd1
d1laj <dimitri.lajou@ens-lyon.fr>
2016-03-11 09:18:53 +0100

    ready to merge

--- classes.py
+++ classes.py
@@ -5,10 +5,8 @@ import math
 
 class Individual:
     """The class independant from Blender describing an individual"""
-    def __init__(self, x, y, z, vmax, vopt, es, ew, radius):
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self, x, y, z, vmax, vopt, es, ew, radius, goal):
+        self.position = S.Point(x, y, z)
         self.vmax = vmax
         self.vopt = vopt
         # See the PLEdestrian paper for the meaning of this notations
@@ -16,10 +14,24 @@ class Individual:
         self.ew = ew
         self.radius = radius
         self.trajectory = list()
+        self.goal = goal
 
 
 class Crowd:
     """The class independant from Blender describing the crowd"""
-    def __init__(self, graph):
-        self.individual = set()
+    def __init__(self, graph, tau):
+        self.individuals = set()
         self.graph = graph
+        self.tau = tau
+    
+    def add_indiv(self, indiv):
+        """Add one individual to the crowd"""
+        self.individuals.add(indiv)
+            
+    def animate(self):
+        """Animate the crowd"""
+        continu = True
+        while continu:
+            continu = False
+            for indiv in self.individuals:
+                
